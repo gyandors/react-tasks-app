b76056178ac4c8cc28c473ee1e31bb7ba7b89aec
Sachin Gyandor <gyandors@gmail.com>
2024-03-17 23:18:43 +0530

    Changing the style of UI dynamically

--- src/Components/TasksInput.js
+++ src/Components/TasksInput.js
@@ -3,21 +3,30 @@ import './TasksInput.css';
 
 export default function TasksInput(props) {
   const [task, setTask] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   function changeHandler(event) {
+    if (event.target.value.trim().length > 0) setIsValid(true);
     setTask(event.target.value);
   }
 
   function formSubmitHandler(event) {
     event.preventDefault();
+    if (task.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     const newTask = { text: task, id: Math.random() };
     props.onAddTask(newTask);
     setTask('');
   }
 
   return (
-    <form className="form-control" onSubmit={formSubmitHandler}>
-      <div className="input-control">
+    <form
+      className={`form-control ${isValid ? '' : 'invalid'}`}
+      onSubmit={formSubmitHandler}
+    >
+      <div className={`input-control ${isValid ? '' : 'invalid'}`}>
         <label htmlFor="task-name">Task Name</label>
         <input
           type="text"
